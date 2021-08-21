338f00e2b9088123db013b680531c6b4d5db3329
Craig Tiller <ctiller@google.com>
2021-08-21 20:16:24 +0000

    interestingnesscheck

--- check.sh
+++ check.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-clang++ -fsanitize=memory -fsanitize-memory-use-after-dtor -o repro repro1.cc
-MSAN_OPTIONS=poison_in_dtor=1 ./repro > log
+clang++ -fsanitize=memory -fsanitize-memory-use-after-dtor -o repro repro1.cc && MSAN_OPTIONS=poison_in_dtor=1 ./repro 2> log || true
 egrep 'SUMMARY: MemorySanitizer: use-of-uninitialized-value .* in grpc_core::Activity::~Activity()' log
 
