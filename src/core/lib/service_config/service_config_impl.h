8ec0a54c07af89bdd1e3e730dd122e7a3c6410fb
ctiller <ctiller@users.noreply.github.com>
2022-02-23 21:52:13 +0000

    Automated change: Fix sanity tests

--- src/core/lib/service_config/service_config_impl.h
+++ src/core/lib/service_config/service_config_impl.h
@@ -124,4 +124,4 @@ class ServiceConfigImpl final : public ServiceConfig {
 
 }  // namespace grpc_core
 
-#endif /* GRPC_CORE_LIB_SERVICE_CONFIG_SERVICE_CONFIG_H */
+#endif /* GRPC_CORE_LIB_SERVICE_CONFIG_SERVICE_CONFIG_IMPL_H */
