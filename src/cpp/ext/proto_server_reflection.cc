f6751ab17c1ce160c28dfb09c4bdd73efa9e58bd
ctiller <ctiller@users.noreply.github.com>
2022-05-18 17:28:00 +0000

    Automated change: Fix sanity tests

--- src/cpp/ext/proto_server_reflection.cc
+++ src/cpp/ext/proto_server_reflection.cc
@@ -25,6 +25,7 @@
 
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/support/interceptor.h>
+#include <grpcpp/support/status_code_enum.h>
 
 using grpc::Status;
 using grpc::StatusCode;
--- src/cpp/server/channelz/channelz_service.cc
+++ src/cpp/server/channelz/channelz_service.cc
@@ -24,6 +24,7 @@
 
 #include <grpc/support/alloc.h>
 #include <grpcpp/impl/codegen/config_protobuf.h>
+#include <grpcpp/support/status_code_enum.h>
 
 namespace grpc {
 
