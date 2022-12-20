482a3e9123487f0a64555dbda5a12fe74bbd0b37
ctiller <ctiller@users.noreply.github.com>
2022-12-20 21:36:48 +0000

    Automated change: Fix sanity tests

--- src/core/lib/gprpp/crash.h
+++ src/core/lib/gprpp/crash.h
@@ -24,11 +24,11 @@
 namespace grpc_core {
 
 // Crash the program after printing `message`.
-// ::grpc_core:: prefix to SourceLocation is required to work around a symbol
- // mismatch bug on MSVC.
+// grpc_core:: prefix to SourceLocation is required to work around a symbol
+// mismatch bug on MSVC.
 [[noreturn]] void Crash(absl::string_view message,
-                        ::grpc_core::SourceLocation location = {});
+                        SourceLocation location = {});
 
-}
+}  // namespace grpc_core
 
 #endif  // GRPC_CORE_LIB_GPRPP_CRASH_H
