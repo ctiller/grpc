671b06cb812e6fbac5188284a5081d11d2da5b78
ctiller <ctiller@users.noreply.github.com>
2023-10-19 18:47:14 +0000

    Automated change: Fix sanity tests

--- src/core/lib/promise/party.h
+++ src/core/lib/promise/party.h
@@ -494,7 +494,7 @@ class Party : public Activity, private Wakeable {
 
     // Outside party poll: check whether the spawning party has completed this
     // promise.
-    ::grpc_core::Poll<Result> PollCompletion() {
+    Poll<Result> PollCompletion() {
       switch (state_.load(std::memory_order_acquire)) {
         case State::kFactory:
         case State::kPromise:
