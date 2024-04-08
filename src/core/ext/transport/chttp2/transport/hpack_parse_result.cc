4f075f73a9e932ff42d4854a674381bacd65d32c
ctiller <ctiller@users.noreply.github.com>
2024-04-08 23:45:45 +0000

    Automated change: Fix sanity tests

--- src/core/ext/transport/chttp2/transport/hpack_parse_result.cc
+++ src/core/ext/transport/chttp2/transport/hpack_parse_result.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <grpc/support/port_platform.h>
+
 #include "src/core/ext/transport/chttp2/transport/hpack_parse_result.h"
 
 #include <stddef.h>
