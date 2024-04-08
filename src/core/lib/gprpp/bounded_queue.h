ae599d508d96dee48db8ba86ee78fec34e5c1780
ctiller <ctiller@users.noreply.github.com>
2024-04-08 23:58:26 +0000

    Automated change: Fix sanity tests

--- src/core/lib/gprpp/bounded_queue.h
+++ src/core/lib/gprpp/bounded_queue.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef BOUNDED_Q_H
-#define BOUNDED_Q_H
+#ifndef GRPC_SRC_CORE_LIB_GPRPP_BOUNDED_QUEUE_H
+#define GRPC_SRC_CORE_LIB_GPRPP_BOUNDED_QUEUE_H
 
 #include <grpc/support/port_platform.h>
 
@@ -65,4 +65,4 @@ class BoundedQueue {
 
 }  // namespace grpc_core
 
-#endif
+#endif  // GRPC_SRC_CORE_LIB_GPRPP_BOUNDED_QUEUE_H
