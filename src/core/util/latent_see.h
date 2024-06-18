772584a7966798b32c741d535e6be17fb31ab305
ctiller <10120821+ctiller@users.noreply.github.com>
2024-06-18 16:38:50 +0000

    Automated change: Fix sanity tests

--- src/core/util/latent_see.h
+++ src/core/util/latent_see.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef LATENT_SEE_H
-#define LATENT_SEE_H
+#ifndef GRPC_SRC_CORE_UTIL_LATENT_SEE_H
+#define GRPC_SRC_CORE_UTIL_LATENT_SEE_H
 
 #include <chrono>
 #include <cstdint>
@@ -183,4 +183,4 @@ struct Flow {};
   grpc_core::latent_see::Flow {}
 #endif
 
-#endif
+#endif  // GRPC_SRC_CORE_UTIL_LATENT_SEE_H
