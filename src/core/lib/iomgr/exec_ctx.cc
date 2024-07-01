770c35dc8ed618c8dba4938ffd70d6914daab026
ctiller <10120821+ctiller@users.noreply.github.com>
2024-07-01 13:25:51 +0000

    Automated change: Fix sanity tests

--- src/core/lib/iomgr/exec_ctx.cc
+++ src/core/lib/iomgr/exec_ctx.cc
@@ -28,7 +28,6 @@
 #include "src/core/lib/gprpp/crash.h"
 #include "src/core/lib/iomgr/combiner.h"
 #include "src/core/lib/iomgr/error.h"
-#include "src/core/lib/iomgr/exec_ctx.h"
 
 static void exec_ctx_run(grpc_closure* closure) {
 #ifndef NDEBUG
